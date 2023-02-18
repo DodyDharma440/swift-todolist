743f4248345aa4c53a4da2e30c0d096847993291
DodyDharma440 <dharma.dody@yahoo.com>
2023-02-18 16:39:24 +0800

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Dodi Aditya on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
